dacc48046bf4b14c2103ab246052a71f23437716
SANT-ANNA Marie-Didier <mariesantanna114@gmail.com>
2021-12-28 18:24:46 +0100

    Gestion historique et consulter

--- app/src/main/java/com/charlesTech8/Historique.java
+++ app/src/main/java/com/charlesTech8/Historique.java
@@ -3,11 +3,11 @@ package com.charlesTech8;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Consulter extends AppCompatActivity {
+public class Historique extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_consulter);
+        setContentView(R.layout.activity_historique);
     }
 }
